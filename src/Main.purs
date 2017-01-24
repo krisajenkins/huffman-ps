9145c58d9eb42caab8e220c8fbcbe48a37a7125f
Kris Jenkins <krisajenkins@gmail.com>
2017-01-24 09:26:53 +0000

    Tidying.

--- src/Main.purs
+++ src/Main.purs
@@ -1,30 +1,34 @@
 module Main where
 
-import Data.Generic (class Generic, gEq)
-import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Map as Map
-import Data.Map (Map)
 import Data.Array as Array
-import Data.List as List
+import Data.Generic (class Generic, gEq)
 import Data.List (List(..),(:))
-import Data.Maybe (Maybe(..))
-import Data.Maybe as Maybe
+import Data.List as List
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Maybe (Maybe(..),maybe)
 import Data.String (fromCharArray, toCharArray)
 import Data.Tuple (Tuple(Tuple))
+import Data.Unfoldable (class Unfoldable)
+import Prelude
 
 frequency :: String -> Map Char Int
 frequency str =
   Array.foldl (flip (Map.alter updater))
       Map.empty
       (toCharArray str)
-    where updater = Maybe.maybe 1 ((+) 1) >>> Just
+    where updater = Just <<< maybe 1 ((+) 1)
 
-toTree :: Map Char Int -> List Tree
-toTree m =
-    (\(Tuple k v) -> Leaf k v) <$> list
-    where list =  Map.toUnfoldable m
+toTree :: forall f. (Functor f, Unfoldable f) => Map Char Int -> f Tree
+toTree map =
+    toLeaf <$> Map.toUnfoldable map
+
+toLeaf :: Tuple Char Int -> Tree
+toLeaf (Tuple k v) = Leaf k v
+
+------------------------------------------------------------
 
 data Tree
   = Leaf Char Int
@@ -32,37 +36,42 @@ data Tree
 
 derive instance genericTree :: Generic Tree
 
+instance eqTree :: Eq Tree where
+  eq = gEq
+
+instance ordTree :: Ord Tree where
+  compare a b = compare (weight a) (weight b)
+
 instance showTree :: Show Tree where
   show = pretty ""
 
 pretty :: String -> Tree -> String
 pretty prefix (Leaf k v) = prefix <> show v <> ": " <> show k
-pretty prefix b@(Branch l r) = pretty newprefix l <> "\n" <> pretty whiteprefix r
-    where newprefix = prefix <> show (weight b) <> "--"
-          whiteprefix = fromCharArray $ const ' ' <$> toCharArray newprefix
-
-instance eqTree :: Eq Tree where
-  eq = gEq
+pretty prefix branch@(Branch l r) =
+  pretty newprefix l <> "\n" <> pretty whiteprefix r
+  where newprefix = prefix <> show (weight branch) <> "--"
+        whiteprefix = fromCharArray $ const ' ' <$> toCharArray newprefix
 
 weight :: Tree -> Int
 weight (Leaf k v) = v
-weight (Branch left right) = (weight left) + (weight right)
+weight (Branch left right) = weight left + weight right
 
-instance ordTree :: Ord Tree where
-  compare a b = compare (weight a) (weight b)
+------------------------------------------------------------
 
 huffman :: List Tree -> Maybe Tree
-huffman xs =
-  step $ List.sort xs
-  where step Nil = Nothing
-        step (Cons x Nil) = Just x
-        step (Cons x (Cons y ys)) = huffman $ if weight x < weight y then (Branch x y : ys) else (Branch y x : ys)
+huffman = step <<< List.sort
+  where
+    step Nil = Nothing
+    step (Cons x Nil) = Just x
+    step (Cons x (Cons y ys)) =
+      huffman $ if weight x < weight y
+                then (Branch x y : ys)
+                else (Branch y x : ys)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = do
   log phrase
-  log $ case huffman probabilities of
-          Nothing -> "Empty"
-          Just ps -> show ps
+  log $ maybe "Empty" show encoded
   where phrase = "Donald Trump is just the best coder. Really great."
         probabilities = toTree $ frequency phrase
+        encoded = huffman probabilities
